f81e5d67108e1aff72bb14964c5def59772f86b6
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-17 12:40:00 +0530

    Working On Js Code

---
+++ script.js
@@ -0,0 +1,19 @@
+// Totals
+const Total = document.getElementById("Total_Income_Value").innerText
+const Expense = document.getElementById("Total_Expense_Value").innerText
+const Balance = document.getElementById("Net_Balance_Value").innerText
+
+// Input Fields
+const Title = document.getElementById("Title").value
+const Amount = document.getElementById("Amount").value
+
+// DropDown
+const Select = document.getElementById("Select")
+
+// Add Button Click
+const Add = document.getElementById("Add")
+
+// Clear Button Click
+const Clear = document.getElementById("Clear")
+
+
