c15bfbcd0848de8c451a6ed37fc26d3d354fb0b2
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-17 22:36:18 +0530

    Working On DOM Manipulation

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 // Totals
-const Total = document.getElementById("Total_Income_Value").innerText
-const Expense = document.getElementById("Total_Expense_Value").innerText
-const Balance = document.getElementById("Net_Balance_Value").innerText
+const Total = document.getElementById("Total_Income_Value")
+const Cost = document.getElementById("Total_Expense_Value")
+const Balance = document.getElementById("Net_Balance_Value")
 
 // Input Fields
-const Title = document.getElementById("Title").value
-const Amount = document.getElementById("Amount").value
+const Title = document.getElementById("Title")
+const Amount = document.getElementById("Amount")
 
 // DropDown
 const Select = document.getElementById("Select")
 
-// Add Button Click
+// Buttons
 const Add = document.getElementById("Add")
-
-// Clear Button Click
 const Clear = document.getElementById("Clear")
 
 
+// Functionalities Start Here
+
+// Data Const
+const Income = 0
+const Expense = 0
+const NetWorth = Income - Expense
+
+Total.innerHTML = Income
+Cost.innerHTML = Expense
+Balance.innerHTML = NetWorth
+
+const Data = []
+
+
+Add.addEventListener("click", () => {
+    // Select.value
+    if ((Title.value).length > 0 && (Amount.value).length > 0) {
+        Data.push({
+            id: Date.now(),
+            "Title": Title.value,
+            "Amount": Amount.value
+        })
+        Title.value = ""
+        Amount.value = ""
+    }
+    else {
+        alert("Enter All The Fields")
+    }
+
+
+})
+
+console.log("Income :", Data)
